a40067c1dabafd1a2d1ed71a0d38313379e6076c
Seth Tal <Seth.tal255@gmail.com>
2025-08-30 13:21:42 -0700

    removed unecessary code from AbilityPickupManager

--- Assets/_Scripts/Entity/AbilityPickupManager.cs
+++ Assets/_Scripts/Entity/AbilityPickupManager.cs
@@ -5,28 +5,13 @@ using UnityEngine;
 public class AbilityPickupManager : SingletonMonoBehavior<AbilityPickupManager>
 {
   [SerializeField, Range(0f, 5f)] private float _pickupCooldown = 1f;
-  [SerializeField, ReadOnly] private List<AbilityPickup> _pickups = new();
-
-  // [Header("Debug")]
-  // [SerializeField, ReadOnly] private bool _isWaiting = false;
 
   /* ---------------------------------------------------------------- */
   /*                           Unity Functions                        */
   /* ---------------------------------------------------------------- */
 
   // private void Awake() {}
-
-  private void Start()
-  {
-    if (Instance == null) return;
-
-    AbilityPickup[] pickups = FindObjectsByType<AbilityPickup>(FindObjectsSortMode.None);
-
-    _pickups = new(pickups);
-
-    // _isWaiting = false;
-  }
-
+  // private void Start() {}
   // private void OnEnable() {}
   // private void OnDisable() {}
   // private void Update() {}
@@ -38,9 +23,6 @@ public class AbilityPickupManager : SingletonMonoBehavior<AbilityPickupManager>
 
   public void StartAbilityPickupCooldown(GameObject pickupGoingOnCooldown)
   {
-    // Don't start another coroutine if we're already running one
-    // if (_isWaiting) return;
-
     StartCoroutine(Wait(_pickupCooldown, pickupGoingOnCooldown));
   }
 
@@ -50,10 +32,8 @@ public class AbilityPickupManager : SingletonMonoBehavior<AbilityPickupManager>
 
   private IEnumerator Wait(float duration, GameObject objectToTempDisable)
   {
-    // _isWaiting = true;
     objectToTempDisable.SetActive(false);
     yield return new WaitForSecondsRealtime(duration);
     objectToTempDisable.SetActive(true);
-    // _isWaiting = false;
   }
 }
