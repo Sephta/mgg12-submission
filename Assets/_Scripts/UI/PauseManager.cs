79a6f6c8564831e96c0755de4935135c0c9faa96
Ash <63310906+ash-chin@users.noreply.github.com>
2025-09-02 17:42:05 -0700

    pause menu work and ability icons

--- Assets/_Scripts/UI/PauseManager.cs
+++ Assets/_Scripts/UI/PauseManager.cs
@@ -1,21 +1,37 @@
 using NaughtyAttributes;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class PauseManager : MonoBehaviour
 {
+
+    [SerializeField] private GameObject _pausePanel;
     [SerializeField, Expandable] private PlayerEventDataSO _playerEventData;
 
 
-    void OnEnable()
+    [SerializeField, ReadOnly] bool _isPaused = false;
+
+
+    private void OnAwake()
+    {
+        if (_playerEventData == null)
+        {
+            Debug.LogError("PlayerEventDataSO required for pause menu.");
+        }
+    }
+
+    private void OnEnable()
     {
         _playerEventData.Pause.OnEventRaised += OnPause;
+        _isPaused = false;  // I know redundant, but I'm paranoid
+        _pausePanel.SetActive(false);
     }
 
     // Update is called once per frame
-    void OnDisable()
+    private void OnDisable()
     {
-
+        _isPaused = false;
     }
 
 
@@ -24,8 +40,38 @@ public class PauseManager : MonoBehaviour
     {
         if (context.started)
         {
-            // stuff do when paused
+            // if we're already paused, they probably want to unpause if they pressed it again
+            if (_isPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
         }
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0;
+        _pausePanel.SetActive(true);
+        _isPaused = true;
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1;
+        _pausePanel.SetActive(false);
+        _isPaused = false;
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1;
+        _pausePanel.SetActive(false);
+        _isPaused = false;
     }
 
 
