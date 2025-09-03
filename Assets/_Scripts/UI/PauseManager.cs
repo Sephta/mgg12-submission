eb897ae2043a4294f41ebaad0560492d6b472cfd
Ash <63310906+ash-chin@users.noreply.github.com>
2025-09-02 23:23:48 -0700

    Pause resume restart and view controls done

--- Assets/_Scripts/UI/PauseManager.cs
+++ Assets/_Scripts/UI/PauseManager.cs
@@ -7,31 +7,53 @@ public class PauseManager : MonoBehaviour
 {
 
     [SerializeField] private GameObject _pausePanel;
+    [SerializeField] private GameObject _controlsPanel;
+    [SerializeField] private GameObject _gamepadControls;
+    [SerializeField] private GameObject _keyboardControls;
+    [SerializeField] private GameObject _gamepadCheck;
+    [SerializeField] private GameObject _gamepadX;
+    [SerializeField] private GameObject _keyboardCheck;
+    [SerializeField] private GameObject _keyboardX;
+
     [SerializeField, Expandable] private PlayerEventDataSO _playerEventData;
 
 
     [SerializeField, ReadOnly] bool _isPaused = false;
 
+    private bool _isShowingGamepadControls = true;
+    private bool _isShowingKeyboardControls = false;
 
-    private void OnAwake()
+    private void Awake()
     {
         if (_playerEventData == null)
         {
-            Debug.LogError("PlayerEventDataSO required for pause menu.");
+            gameObject.SetActive(false);
         }
     }
 
     private void OnEnable()
     {
-        _playerEventData.Pause.OnEventRaised += OnPause;
+        if (_playerEventData != null)
+        {
+            _playerEventData.Pause.OnEventRaised += OnPause;
+        }
+
         _isPaused = false;  // I know redundant, but I'm paranoid
         _pausePanel.SetActive(false);
+        _controlsPanel.SetActive(false);
     }
 
     // Update is called once per frame
     private void OnDisable()
     {
+        if (_playerEventData != null)
+        {
+            _playerEventData.Pause.OnEventRaised -= OnPause;
+        }
+
         _isPaused = false;
+        _pausePanel.SetActive(false);
+        _controlsPanel.SetActive(false);
     }
 
 
@@ -64,6 +86,7 @@ public class PauseManager : MonoBehaviour
         Time.timeScale = 1;
         _pausePanel.SetActive(false);
         _isPaused = false;
+        if (_controlsPanel.activeSelf) _controlsPanel.SetActive(false);
     }
 
     public void Restart()
@@ -73,6 +96,94 @@ public class PauseManager : MonoBehaviour
         _pausePanel.SetActive(false);
         _isPaused = false;
     }
+
+    public void ShowControls()
+    {
+        Debug.Log("wtf?");
+        // Time.timeScale = 1;
+        _pausePanel.SetActive(false);
+        _controlsPanel.SetActive(true);
+        SwapGamepadControlsVisibility(_isShowingGamepadControls);
+        SwapMKBControlsVisibility(_isShowingKeyboardControls);
+        // _gamepadControls.SetActive(true);
+        // _gamepadCheck.SetActive(_isShowingGamepadControls);
+        // _gamepadX.SetActive(!_isShowingGamepadControls);
+        // _keyboardCheck.SetActive(_isShowingKeyboardControls);
+        // _keyboardX.SetActive(!_isShowingKeyboardControls);
+    }
+
+    public void ExitControls()
+    {
+        _controlsPanel.SetActive(false);
+        _pausePanel.SetActive(true);
+    }
+
+
+    // look this is how I made the UI look whatever lol
+    public void ShowGamepadControls()
+    {
+        Debug.Log("HELLO");
+        if (!_isShowingGamepadControls)
+        {
+            SwapGamepadControlsVisibility(true);
+            SwapMKBControlsVisibility(false);
+            _isShowingGamepadControls = true;
+            _isShowingKeyboardControls = false;
+
+        }
+        else
+        {
+            SwapGamepadControlsVisibility(false);
+            SwapMKBControlsVisibility(true);
+            _isShowingGamepadControls = false;
+            _isShowingKeyboardControls = true;
+        }
+    }
+
+    public void ShowMKBControls()
+    {
+        Debug.Log("HELLO MKB");
+
+        if (!_isShowingKeyboardControls)
+        {
+            SwapGamepadControlsVisibility(false);
+            SwapMKBControlsVisibility(true);
+            _isShowingGamepadControls = false;
+            _isShowingKeyboardControls = true;
+
+        }
+        else
+        {
+            SwapGamepadControlsVisibility(true);
+            SwapMKBControlsVisibility(false);
+            _isShowingGamepadControls = true;
+            _isShowingKeyboardControls = false;
+        }
+    }
+
+
+    public void Quit()
+    {
+        Debug.Log("Quitting the game :C");
+        Application.Quit();
+    }
+
+
+    private void SwapGamepadControlsVisibility(bool selectShow)
+    {
+        _gamepadControls.SetActive(selectShow);
+        _gamepadCheck.SetActive(selectShow);
+        _gamepadX.SetActive(!selectShow);
+    }
+
+    private void SwapMKBControlsVisibility(bool selectShow)
+    {
+        _keyboardControls.SetActive(selectShow);
+        _keyboardCheck.SetActive(selectShow);
+        _keyboardX.SetActive(!selectShow);
+    }
+
+
 
 
 }
