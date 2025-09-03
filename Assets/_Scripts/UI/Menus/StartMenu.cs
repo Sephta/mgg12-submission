eb56a2409b83753288d550cef59be66af2266be4
Ash <63310906+ash-chin@users.noreply.github.com>
2025-09-03 12:43:10 -0700

    basic main menu done and some reorg of the scripts UI folder
    There is a thing about build profiles in here, but I deleted the build profile so ignore it

---
+++ Assets/_Scripts/UI/Menus/StartMenu.cs
@@ -0,0 +1,57 @@
+using System;
+using LDtkUnity;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+    [SerializeField] private bool _isSplashImagePresent = false;
+    [SerializeField] private bool _isTesting = false;
+    [SerializeField] private GameObject _howToPlayScreen;
+    [SerializeField] private GameObject _splashImage;
+    [SerializeField] private GameObject _debugButtons;
+
+    private void OnEnable()
+    {
+        _howToPlayScreen.SetActive(false);
+        _splashImage.SetActive(_isSplashImagePresent);
+        _debugButtons.SetActive(_isTesting);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("World Map");
+    }
+
+    public void HowToPlay()
+    {
+        if (!_howToPlayScreen.activeSelf)
+        {
+            _howToPlayScreen.SetActive(true);
+            if (_splashImage.activeSelf) _splashImage.SetActive(false);
+        }
+        else
+        {
+            _howToPlayScreen.SetActive(false);
+            _splashImage.SetActive(_isSplashImagePresent);
+        }
+
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Quitting the game :C");
+        Application.Quit();
+    }
+
+    public void LoadSethScene()
+    {
+        SceneManager.LoadScene("Stal_prototyping_bramble_system");
+    }
+
+    public void LoadAshScene()
+    {
+        SceneManager.LoadScene("Ash_Sandbox");
+    }
+}
