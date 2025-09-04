71c121828d64c12f3e47cf134bd1d8fc8e4e74e2
Seth Tal <Seth.tal255@gmail.com>
2025-09-03 20:40:13 -0700

    Fixed Camera Shake, it should work now hopefully

--- Assets/_Scripts/Camera/CameraShake.cs
+++ Assets/_Scripts/Camera/CameraShake.cs
@@ -6,10 +6,11 @@ using UnityEngine;
 public class CameraShake : MonoBehaviour
 {
   [SerializeField] private CinemachineCamera _camera = null;
+  [SerializeField] private CinemachineBasicMultiChannelPerlin _multiChannelPerlin;
   [SerializeField] private FloatFloatEventChannelSO _cameraShakeEvent;
-  [SerializeField] private float shakeIntensity = 0f;
-  [SerializeField] private float shakeTimer = 0f;
-  [SerializeField, ReadOnly] private float currShakeTime = 0f;
+  [SerializeField] private float _shakeIntensity = 0f;
+  [SerializeField] private float _shakeTimer = 0f;
+  [SerializeField, ReadOnly] private float _currentShakeTime = 0f;
 
   /* ---------------------------------------------------------------- */
   /*                           Unity Functions                        */
@@ -22,6 +23,11 @@ public class CameraShake : MonoBehaviour
 
   private void OnEnable()
   {
+    if (_camera != null)
+    {
+      _multiChannelPerlin = _camera.GetComponent<CinemachineBasicMultiChannelPerlin>();
+    }
+
     if (_cameraShakeEvent == null) return;
 
     _cameraShakeEvent.OnEventRaised += ShakeCamera;
@@ -36,16 +42,13 @@ public class CameraShake : MonoBehaviour
 
   private void Update()
   {
-    if (currShakeTime >= 0)
+    if (_currentShakeTime >= 0)
     {
-      currShakeTime = Mathf.Clamp(currShakeTime - Time.deltaTime, 0f, shakeTimer);
+      _currentShakeTime = Mathf.Clamp(_currentShakeTime - Time.deltaTime, 0f, _shakeTimer);
 
-      if (currShakeTime <= 0f)
+      if (_currentShakeTime <= 0f && _multiChannelPerlin != null)
       {
-        CinemachineBasicMultiChannelPerlin mChannelPerlin =
-          _camera.GetComponentInChildren<CinemachineBasicMultiChannelPerlin>();
-
-        mChannelPerlin.AmplitudeGain = 0f;
+        _multiChannelPerlin.AmplitudeGain = 0f;
       }
     }
   }
@@ -54,22 +57,23 @@ public class CameraShake : MonoBehaviour
   /*                               PUBLIC                             */
   /* ---------------------------------------------------------------- */
   [Button("Shake Camera")]
-  public void ShakeCamera()
+  public void ShakeCameraNoParams()
   {
-    CinemachineBasicMultiChannelPerlin mChannelPerlin =
-      _camera.GetComponentInChildren<CinemachineBasicMultiChannelPerlin>();
+    if (_multiChannelPerlin == null) return;
 
-    mChannelPerlin.AmplitudeGain = shakeIntensity;
-    currShakeTime = shakeTimer;
+    _multiChannelPerlin.AmplitudeGain = _shakeIntensity;
+    _currentShakeTime = _shakeTimer;
   }
 
   public void ShakeCamera(float intensity, float duration)
   {
-    CinemachineBasicMultiChannelPerlin mChannelPerlin =
-      _camera.GetComponentInChildren<CinemachineBasicMultiChannelPerlin>();
+    if (_multiChannelPerlin == null) return;
+    Debug.Log("ShakeCamera <" + intensity + ": " + duration + ">");
 
-    mChannelPerlin.AmplitudeGain = intensity;
-    currShakeTime = duration;
+    _multiChannelPerlin.AmplitudeGain = intensity;
+    _shakeTimer = duration;
+    _shakeIntensity = intensity;
+    _currentShakeTime = duration;
   }
 
   /* ---------------------------------------------------------------- */
@@ -79,14 +83,11 @@ public class CameraShake : MonoBehaviour
   [Button("Reset")]
   private void ResetCameraShake()
   {
-    shakeIntensity = 0f;
+    _shakeIntensity = 0f;
+    _shakeTimer = 0f;
+    _currentShakeTime = 0f;
 
-    CinemachineBasicMultiChannelPerlin mChannelPerlin =
-      _camera.GetComponentInChildren<CinemachineBasicMultiChannelPerlin>();
-
-    mChannelPerlin.AmplitudeGain = shakeIntensity;
-
-    shakeTimer = 0f;
-    currShakeTime = 0f;
+    if (_multiChannelPerlin == null) return;
+    _multiChannelPerlin.AmplitudeGain = _shakeIntensity;
   }
 }
